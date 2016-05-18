07c956e018b0e45f76e7ea912cdff9f9e95aa0b9
Kaspars Mickevics <kaspars@fx.lv>
2016-05-18 21:13:24 +0300

    Accept target option

--- udpsender.c
+++ udpsender.c
@@ -5,8 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h> 
+#include <unistd.h>
 
-int main(void)
+void usage()
+{
+    printf("Usage:\n");
+    printf("./udpsender -t <target>\n\n");
+}
+
+int main(int argc, char *argv[])
 {
     int s;
     int port = 200;
@@ -14,19 +21,37 @@ int main(void)
     char *msg;
     struct sockaddr_in serveraddr;
     struct hostent *server;
-    
+
+    int opt;
+    extern char *optarg;
+    char *target;
+
+    while((opt = getopt(argc, argv, "t:")) != -1){
+        switch(opt){
+            case 't':
+                target = optarg;
+                break;
+            default:
+                usage();
+        }
+    }
+
+    if(target == NULL){
+        target = "fx.lv"; // set default target
+    }
+
+    msg = "Hi!"; // default message
+
     s = socket(AF_INET, SOCK_DGRAM, 0);
 
-    hostname = "fx.lv";
-    msg = "Hi!";
 
-    server = gethostbyname(hostname);
+    server = gethostbyname(target);
 
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, 
     (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(port);
-
+    printf("Sending packet to %s:%d\n", target, port);
     sendto(s, msg, strlen(msg), 0, &serveraddr, sizeof(serveraddr));
 }
